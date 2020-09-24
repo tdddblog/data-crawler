c8428a83494b496f85a63ca0f69e9f4f2046297c
Karpenko <ykarpenk@ndc.nasa.gov>
2020-09-24 10:38:33 -0700

    'Refactoring'

---
+++ src/ek/parser.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self, base_path):
+        super().__init__()
+        self.files = set()
+        self.dirs = set()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            href = attrs[0][1]
+            if href.endswith(".xml"):
+                self.files.add(href)
+            elif href.endswith("/") and not href.startswith("/"):
+                self.dirs.add(href)
+
+    def close(self):
+        super().close()
